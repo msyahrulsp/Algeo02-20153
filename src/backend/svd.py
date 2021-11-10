135cb3c633d8e02780d9c3d341ede0eb4c0e840e
vito <vitosgh@gmail.com>
2021-11-11 00:58:15 +0700

    Change method to QR Iteration

--- src/backend/svd.py
+++ src/backend/svd.py
@@ -9,35 +9,24 @@ image = Image.open('src/backend/lena_image.jpg')
 array_image = np.asarray(image)
 array_image_transpose = array_image.transpose()
 
-def zeros_matrix(rows, cols):
-    #fungsi buat bikin matriks yang isi nya nol
-    #berfungsi buat nyari nilai eigen nantinya
-    M = []
-    while len(M) < rows:
-        M.append([])
-        while len(M[-1]) < cols:
-            M[-1].append(0)
-    return M
 
-def identity_matrix(n):
-    #fungsi buat bikin matriks identitas
-    #kepake nanti buat nyari nilai eigen value
-    IdM = zeros_matrix(n,n)
-    for i in range(n):
-        IdM[i][i] = 1
-    return IdM
+def simultaneous_power_iteration(A: np.ndarray, k: int):
+    n, m = A.shape
+    Q = np.random.rand(n, k)
+    Q, _ = np.linalg.qr(Q)
+    Q_prev = Q
+ 
+    for i in range(1000):
+        Z = A.dot(Q)
+        Q, R = np.linalg.qr(Z)
 
+        # can use other stopping criteria as well 
+        err = ((Q - Q_prev) ** 2).sum()
+        if i % 100 == 0:
+            print(i, err) # untuk debug, bisa dihapus
 
+        Q_prev = Q
+        if err < 1e-3:
+            break
 
-#mencari nilai eigen dari singular kiri
-aaT = np.matmul(array_image,array_image_transpose)
-
-
-#mencari nilai eigen dari singular kanan
-aTa = np.matmul(array_image_transpose,array_image)
-
-
-
-#print(arr)
-#print("transpose")
-#print(arrT)
+    return np.diag(R), Q
